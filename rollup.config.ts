0fe1d5cf0d56ed12c077bec309e71adff4500c8d
snewcomer <snewcomer24@gmail.com>
2019-12-26 22:02:33 -0800

    fix rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -5,7 +5,7 @@ const pkg = require('./package.json');
 const libraryName = 'validatedChangeset';
 
 export default {
-  input: 'src/index.ts',
+  input: 'dist/es/index.ts',
   output: [
     { file: pkg.main, name: libraryName, format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true }
