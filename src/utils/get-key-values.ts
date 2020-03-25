ce85db1c198fa1bd5f1143401ee63cd1fcc630b0
snewcomer <snewcomer24@gmail.com>
2020-03-24 20:34:34 -0700

    Properly serialize getKeyErrorValues with nested

--- src/utils/get-key-values.ts
+++ src/utils/get-key-values.ts
@@ -27,40 +27,34 @@ export function getKeyValues<T extends Record<string, any>>(
   return map;
 }
 
-let errorKeysUpToValue: string[] = [];
-
 /**
  * traverse through target and return leaf nodes with `value` property and key as 'person.name'
  *
  * @method getKeyErrorValues
  * @return {Array} [{ key: 'person.name', validation: '', value: '' }]
  */
-export function getKeyErrorValues<T extends Record<string, any>>(obj: T): PublicErrors {
+export function getKeyErrorValues<T extends Record<string, any>>(
+  obj: T,
+  keysUpToValue: Array<string> = []
+): PublicErrors {
   let map = [];
 
   for (let key in obj) {
-    errorKeysUpToValue.push(key);
-
     if (obj[key] && isObject(obj[key])) {
       if (
         Object.prototype.hasOwnProperty.call(obj[key], 'value') &&
         (obj[key] as any) instanceof Err
       ) {
         map.push({
-          key: errorKeysUpToValue.join('.'),
+          key: [...keysUpToValue, key].join('.'),
           validation: obj[key].validation,
           value: obj[key].value
         });
-        // stop collecting keys
-        errorKeysUpToValue = [];
       } else if (key !== 'value') {
-        map.push(...getKeyErrorValues(obj[key]));
+        map.push(...getKeyErrorValues(obj[key], [...keysUpToValue, key]));
       }
     }
   }
-
-  // reset
-  errorKeysUpToValue = [];
 
   return map;
 }
--- test/utils/get-key-values.test.ts
+++ test/utils/get-key-values.test.ts
@@ -1,4 +1,5 @@
-import { getKeyValues } from '../../src/utils/get-key-values';
+import { getKeyValues, getKeyErrorValues } from '../../src/utils/get-key-values';
+import Err from '../../src/-private/err';
 
 describe('Unit | Utility | getKeyValues', function() {
   it('it works with single level values', () => {
@@ -24,6 +25,35 @@ describe('Unit | Utility | getKeyValues', function() {
       { key: 'user.lastName', value: 'Bolton' },
       { key: 'user.address.city', value: 'NYC' },
       { key: 'user.address.state', value: 'New York' }
+    ]);
+  });
+});
+
+describe('Unit | Utility | getKeyErrorValues', function() {
+  it('it works with single level values', () => {
+    const result = getKeyErrorValues({
+      test: new Err(1, 'foobar')
+    });
+
+    expect(result).toEqual([{ key: 'test', value: 1, validation: 'foobar' }]);
+  });
+
+  it('it works with nested keys', () => {
+    const result = getKeyErrorValues({
+      user: {
+        firstName: new Err('Michael', 'Jordan'),
+        lastName: { value: 'Bolton' },
+        address: {
+          city: new Err('NYC', 'dungeon'),
+          state: new Err('New York', 'grassland')
+        }
+      }
+    });
+
+    expect(result).toEqual([
+      { key: 'user.firstName', value: 'Michael', validation: 'Jordan' },
+      { key: 'user.address.city', value: 'NYC', validation: 'dungeon' },
+      { key: 'user.address.state', value: 'New York', validation: 'grassland' }
     ]);
   });
 });
