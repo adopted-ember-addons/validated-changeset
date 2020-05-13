28d1865e0a69a9d7f91eee9c2542affa44064c27
Scott Newcomer <snewcomer24@gmail.com>
2020-05-12 21:25:40 -0700

    Revert "Merge sibling keys with `get`"
    This reverts commit fc7c759b9d2fa9a32a9197d388e15ab9da3c35f7.

--- src/utils/is-leaf.ts
+++
@@ -1,7 +0,0 @@
-import getDeep from './get-deep';
-import Change from '../-private/change';
-import { Changes } from '../types';
-
-export function isLeafInChanges(key: string, changes: Changes) {
-  return getDeep(changes, key) && getDeep(changes, key) instanceof Change;
-}
