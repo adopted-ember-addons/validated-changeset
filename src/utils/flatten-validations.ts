af4f23f83256010b861c6b8d8806b4d920a69ff9
Scott Newcomer <snewcomer24@gmail.com>
2020-05-16 15:16:02 -0700

    Validate nested keys with validate/0

---
+++ src/utils/flatten-validations.ts
@@ -0,0 +1,36 @@
+import { ValidatorMap } from '../types';
+import isObject from './is-object';
+
+function flatten(
+  validatorMap: Record<string, any>,
+  obj: Record<string, any>,
+  keys: string[],
+  keysUpToFunction: Array<string> = []
+): object {
+  for (let key of keys) {
+    const value: any = validatorMap[key];
+    if (isObject(value)) {
+      flatten(value, obj, Object.keys(value), [...keysUpToFunction, key]);
+    } else if (typeof value === 'function') {
+      const dotSeparatedKeys = [...keysUpToFunction, key].join('.');
+      obj[dotSeparatedKeys] = value;
+    } else if (Array.isArray(value)) {
+      const allFuncs: boolean = value.every(item => typeof item === 'function');
+      if (allFuncs) {
+        const dotSeparatedKeys = [...keysUpToFunction, key].join('.');
+        obj[dotSeparatedKeys] = value;
+      }
+    }
+  }
+
+  return obj;
+}
+
+export function flattenValidations(validatorMap: Record<string, any>): object {
+  if (!validatorMap) {
+    return {};
+  }
+
+  let obj: Record<string, any> = {};
+  return flatten(validatorMap, obj, Object.keys(validatorMap));
+}
---
+++ test/utils/flatten-validations.test.ts
@@ -0,0 +1,45 @@
+import { flattenValidations } from '../../src/utils/flatten-validations';
+
+describe('Unit | Utility | flattenValidations', function() {
+  it('it works with single level values', () => {
+    const testFunc = () => {};
+    const input = { testFunc };
+    const result: Record<string, any> = flattenValidations(input);
+
+    expect(result.testFunc).toEqual(testFunc);
+  });
+
+  it('it works with nested keys', () => {
+    const nyFunc = () => {};
+    const input = {
+      usa: {
+        ny: nyFunc
+      }
+    };
+    const result: Record<string, any> = flattenValidations(input);
+
+    expect(result['usa.ny']).toEqual(nyFunc);
+  });
+
+  it('it works with nested keys', () => {
+    const nyFunc = () => {};
+    const mnFunc = () => {};
+    const paulFunc = () => {};
+    const groveFunc = () => {};
+    const input = {
+      usa: {
+        ny: [nyFunc, mnFunc],
+        mn: {
+          stpaul: paulFunc,
+          grove: groveFunc
+        }
+      }
+    };
+    const result: Record<string, any> = flattenValidations(input);
+
+    expect(result.usa).toBeUndefined();
+    expect(result['usa.ny']).toEqual([nyFunc, mnFunc]);
+    expect(result['usa.mn.stpaul']).toEqual(paulFunc);
+    expect(result['usa.mn.grove']).toEqual(groveFunc);
+  });
+});
