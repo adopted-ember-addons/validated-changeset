ca364a790194634d0e068ac855008ea91b4c4a75
Scott Newcomer <snewcomer24@gmail.com>
2020-09-03 21:11:39 -0700

    change name

--- src/utils/build-old-values.ts
+++ src/utils/build-old-values.ts
@@ -1,12 +1,12 @@
 export function buildOldValues(
   content: any,
   changes: Record<string, any>[],
-  deepGet: Function
+  getDeep: Function
 ): object {
   const obj = Object.create(null);
 
   for (let change of changes) {
-    obj[change.key] = deepGet(content, change.key);
+    obj[change.key] = getDeep(content, change.key);
   }
 
   return obj;
