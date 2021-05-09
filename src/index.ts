b08b4aab7d8f548f403e79658187e186eece110e
scott-newcomer <snewcomer24@gmail.com>
2021-05-09 15:02:48 -0500

    try this

--- src/index.ts
+++ src/index.ts
@@ -959,14 +959,12 @@ export class BufferedChangeset implements IChangeset {
             return getChangeValue(result);
           }
 
-          const baseContent = this.safeGet(content, baseKey);
-          if (baseContent) {
-            const subContent = this.getDeep(baseContent, remaining.join('.'));
-            const subChanges = getSubObject(changes, key);
-            // give back an object that can further retrieve changes and/or content
-            const tree = new ObjectTreeNode(subChanges, subContent, this.getDeep, this.isObject);
-            return tree.proxy;
-          }
+          const baseContent = this.safeGet(content, baseKey) || {};
+          const subContent = this.getDeep(baseContent, remaining.join('.'));
+          const subChanges = getSubObject(changes, key);
+          // give back an object that can further retrieve changes and/or content
+          const tree = new ObjectTreeNode(subChanges, subContent, this.getDeep, this.isObject);
+          return tree.proxy;
         } else if (typeof result !== 'undefined') {
           return result;
         }
