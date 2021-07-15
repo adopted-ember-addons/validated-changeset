9f0a8aed96e836d3d52f8dc3483e0acc21600608
Scott Newcomer <snewcomer24@gmail.com>
2021-07-15 07:28:07 -0500

    Bugfix: ensure changesetKeys is passed through in Ember versions > 3.24

--- src/index.ts
+++ src/index.ts
@@ -107,7 +107,8 @@ export class BufferedChangeset implements IChangeset {
     this[ERRORS] = {};
     this[ERRORS_CACHE] = {};
     this[VALIDATOR] = validateFn;
-    this[OPTIONS] = pureAssign(defaultOptions, options);
+    // separate assign due Proxy object for options
+    this[OPTIONS] = pureAssign(defaultOptions, options, { changesetKeys: options.changesetKeys });
     this[RUNNING_VALIDATIONS] = {};
   }
 
