30e1d51dc70f8bf34fd891ded1570a6ed72ff116
Jeff Keen <jeff@keen.me>
2025-03-20 10:14:52 -0500

    Switch project to pnpm, update dependencies and config to avoid error

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,8 +7,17 @@ const libraryName = 'validatedChangeset';
 export default {
   input: 'dist/es/index.js',
   output: [
-    { file: pkg.main, name: libraryName, format: 'umd', sourcemap: true },
+    {
+      file: pkg.main,
+      name: libraryName,
+      format: 'umd',
+      sourcemap: true,
+      globals: {
+        '@ungap/structured-clone': 'structuredClone' // Define a global name
+      }
+    },
     { file: pkg.module, format: 'es', sourcemap: true }
   ],
+  external: ['@ungap/structured-clone'],
   plugins: [typescript(), sourceMaps()]
 };
